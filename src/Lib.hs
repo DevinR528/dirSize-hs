cf9ca6ac471eecdd6327f3caa1501bec11d5b31a
Devin Ragotzy <devin.ragotzy@gmail.com>
2019-11-09 19:42:34 -0500

    working correctly filters files and dirs

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,9 +3,30 @@ module Lib
     ) where
 import System.Directory
 
+flatten :: [[a]] -> [a]
+flatten [[]] = []
+flatten [[a]] = [a]
+flatten (x:xs) = x ++ flatten xs
+
+folderOrFile :: FilePath -> IO [FilePath]
+folderOrFile path = do
+  isFile <- doesFileExist path
+  if isFile
+    then return [path]
+    else do
+      isFolder <- doesDirectoryExist path
+      if isFolder
+        then do
+          xs <- listDirectory path
+          nested <- mapM folderOrFile xs
+          return (flatten nested)
+        else
+          return []
+
 directorySize :: String -> IO ()
 directorySize folder = do
   paths <- listDirectory folder
-  sizes <- mapM getFileSize paths
+  x <- mapM folderOrFile paths
+  sizes <- mapM getFileSize (flatten x)
   let count = sum sizes
   print count
